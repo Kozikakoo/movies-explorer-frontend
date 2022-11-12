2786be0b76bf640fc474c524ea712775e9417536
Anastasiya Nagornaya <kozikakoo@yandex.ru>
2022-11-12 21:36:10 +0300

    feat: adaptive 320

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,9 +5,9 @@ function Portfolio() {
         <section className="portfolio">
             <h2 className="portfolio__title">Портфолио</h2>
             <ul className="portfolio__list">
-                <li className="portfolio__item"><a className="portfolio__link" href="#">Статичный сайт</a><div className="portfolio__img-link"></div></li>
-                <li className="portfolio__item"><a className="portfolio__link" href="#">Адаптивный сайт</a><div className="portfolio__img-link"></div></li>
-                <li className="portfolio__item"><a className="portfolio__link" href="#">Одностраничное приложение</a><div className="portfolio__img-link"></div></li>
+                <li className="portfolio__item"><a className="portfolio__link" href="https://kozikakoo.github.io/how-to-learn/" target="_blank">Статичный сайт</a><div className="portfolio__img-link"></div></li>
+                <li className="portfolio__item"><a className="portfolio__link" href="https://kozikakoo.github.io/russian-travel/index.html" target="_blank">Адаптивный сайт</a><div className="portfolio__img-link"></div></li>
+                <li className="portfolio__item"><a className="portfolio__link" href="https://kozikakoo.github.io/mesto/index.html" target="_blank">Одностраничное приложение</a><div className="portfolio__img-link"></div></li>
             </ul>
         </section>
     )
--- src/components/Promo/Promo.js
+++ src/components/Promo/Promo.js
@@ -4,7 +4,7 @@ function Promo(props) {
 
     return(
         <div className="promo">
-            <h1 className="promo__text">Учебный проект&nbsp;студента факультета Веб&#8209;разработки.</h1>
+            <h1 className="promo__text">Учебный&nbsp;проект студента факультета Веб&#8209;разработки.</h1>
             {props.children}
         </div>
     )
