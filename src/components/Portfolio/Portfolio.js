41803054607f709975330fe96c8b99e793cf66db
Anastasiya Nagornaya <kozikakoo@yandex.ru>
2022-11-14 15:30:28 +0300

    fix: header/title, portfolio link, techs

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -5,9 +5,9 @@ function Portfolio() {
         <section className="portfolio">
             <h2 className="portfolio__title">Портфолио</h2>
             <ul className="portfolio__list">
-                <li className="portfolio__item"><a className="portfolio__link" href="https://kozikakoo.github.io/how-to-learn/" target="_blank">Статичный сайт</a><div className="portfolio__img-link"></div></li>
-                <li className="portfolio__item"><a className="portfolio__link" href="https://kozikakoo.github.io/russian-travel/index.html" target="_blank">Адаптивный сайт</a><div className="portfolio__img-link"></div></li>
-                <li className="portfolio__item"><a className="portfolio__link" href="https://kozikakoo.github.io/mesto/index.html" target="_blank">Одностраничное приложение</a><div className="portfolio__img-link"></div></li>
+                <li><a className="portfolio__link" href="https://kozikakoo.github.io/how-to-learn/" target="_blank">Статичный сайт<div className="portfolio__img-link"></div></a></li>
+                <li><a className="portfolio__link" href="https://kozikakoo.github.io/russian-travel/index.html" target="_blank">Адаптивный сайт<div className="portfolio__img-link"></div></a></li>
+                <li><a className="portfolio__link" href="https://kozikakoo.github.io/mesto/index.html" target="_blank">Одностраничное приложение<div className="portfolio__img-link"></div></a></li>
             </ul>
         </section>
     )
